5386609e0ba54fe7d7fce2e7ea11f9a40f0d47eb
Mayank <nailwalmayank3@gmail.com>
2026-04-26 19:25:30 +0530

    restarting the process 5

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,15 @@ app.use(express.json());
 
 let messages = [];
 
-// API routes
+// GET messages
 app.get("/api/message", (req, res) => {
   res.json(messages);
 });
 
+// POST message
 app.post("/api/message", (req, res) => {
   messages.push({ text: req.body.text });
   res.sendStatus(200);
-});
-
-// health check (for later DevOps feature)
-app.get("/health", (req, res) => {
-  res.json({ status: "OK" });
 });
 
 app.listen(5000, () => {
